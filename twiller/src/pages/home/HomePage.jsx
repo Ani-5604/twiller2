6a41e13e49c529e6c3d28612245c939c8fe4c187
Ani-5604 <151756510+Ani-5604@users.noreply.github.com>
2024-12-14 11:04:31 +0530

    Update HomePage.jsx

--- twiller/src/pages/home/HomePage.jsx
+++ twiller/src/pages/home/HomePage.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 
-import Posts from "../../components/common/Posts";
-import CreatePost from "./CreatePost";
-import Widgets from "../Widgets/Widgets";
 
 const HomePage = () => {
   const [feedType, setFeedType] = useState("forYou");
@@ -13,7 +10,7 @@ const HomePage = () => {
       <div className="flex-[4_4_0] mr-auto border-r border-sky-700 min-h-screen">
      
             <p>hello</p>
-          <Widgets />
+        
       </div>
     </>
   );
