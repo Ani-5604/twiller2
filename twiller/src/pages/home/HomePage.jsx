4c680752d86659ef6ef338bdc1520b744f4fd0fa
Ani-5604 <151756510+Ani-5604@users.noreply.github.com>
2024-12-14 11:02:35 +0530

    Update HomePage.jsx

--- twiller/src/pages/home/HomePage.jsx
+++ twiller/src/pages/home/HomePage.jsx
@@ -14,30 +14,8 @@ const HomePage = () => {
         {/* Header */}
         <div className="flex w-full border-b border-sky-700">
           <div
-            className={
-              "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-            }
-            onClick={() => setFeedType("forYou")}
-          >
-            {("For you")} {/* This will display "Pour vous" in French */}
-            {feedType === "forYou" && (
-              <div className="absolute bottom-0 w-10 h-1 rounded-full bg-primary"></div>
-            )}
-          </div>
-          <div
-            className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
-            onClick={() => setFeedType("following")}
-          >
-            {("Following")} {/* This will display "Abonnements" in French */}
-            {feedType === "following" && (
-              <div className="absolute bottom-0 w-10 h-1 rounded-full bg-primary"></div>
-            )}
-          </div>
-        </div>
-        <CreatePost />
-        {/* POSTS */}
-        <Posts feedType={feedType} />
-        <Widgets />
+            <p>hello</p>
+          <Widgets />
       </div>
     </>
   );
